d34930adf8b634f2f09ad48a697a9fd1c1d786e4
swadhinjit-das <swadhinjit@credencesoft.in>
2026-06-25 13:09:57 +0530

    Update HomePageClient.tsx

--- components/HomePageClient.tsx
+++ components/HomePageClient.tsx
@@ -2,5 +2,5 @@
 
 import dynamic from "next/dynamic";
 
-export const CinematicHero = dynamic(() => import("@/components/hero/CinematicHero"), { ssr: false });
+export const CinematicHero = dynamic(() => import("@/components/hero/CinematicHero"));
 export const RoomCard = dynamic(() => import("@/components/rooms/RoomCard"), { ssr: false });
